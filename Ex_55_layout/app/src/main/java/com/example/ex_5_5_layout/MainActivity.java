57807f06fd13b4e821b1afa1b4cb94afdd0504be
wang-hsoo <64819868+wang-hsoo@users.noreply.github.com>
2021-10-15 14:24:55 +0900

    Add files via upload

---
+++ Ex_55_layout/app/src/main/java/com/example/ex_5_5_layout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ex_5_5_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
