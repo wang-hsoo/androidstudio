0b5b2f2c9b38fde41e283e78dfe8f92d140e05e2
wang-hsoo <64819868+wang-hsoo@users.noreply.github.com>
2021-11-06 10:01:19 +0900

    Add files via upload

---
+++ tabHost_main/java/com/example/tabhost/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tabhost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpec1 = tabHost.newTabSpec("TAG1").setIndicator("강아지");
+        tabSpec1.setContent(R.id.imageView1);
+        tabHost.addTab(tabSpec1);
+
+        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("TAG2").setIndicator("고양이");
+        tabSpec2.setContent(R.id.imageView2);
+        tabHost.addTab(tabSpec2);
+
+        TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("TAG3").setIndicator("토끼");
+        tabSpec3.setContent(R.id.imageView3);
+        tabHost.addTab(tabSpec3);
+
+        TabHost.TabSpec tabSpec4 = tabHost.newTabSpec("TAG4").setIndicator("말");
+        tabSpec4.setContent(R.id.imageView4);
+        tabHost.addTab(tabSpec4);
+
+
+
+
+    }
+}
